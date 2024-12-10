31a52d7d69a8cb2637de9c1070d228a0f3288c1d
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-10 15:49:46 -0300

    10DEZ24 - Aula 110 AS - Navegando pelos Fragments (beginTransaction; add; replace; remove; commit).

--- app/src/main/java/com/arttt95/fragments/MainActivity.kt
+++ app/src/main/java/com/arttt95/fragments/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.arttt95.fragments
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arttt95.fragments.fragments.ChamadasFragment
+import com.arttt95.fragments.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas: Button
+    private lateinit var btnChamadas: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnConversas = findViewById(R.id.btn_conversas)
+        btnChamadas = findViewById(R.id.btn_chamadas)
+
+        /*val fragmentManager = supportFragmentManager.beginTransaction()
+
+        // Diversas alterações em fragments
+        val conversasFragment = ConversasFragment()
+        fragmentManager.add( R.id.fragment_conteudo, conversasFragment )
+
+        fragmentManager.commit()*/
+
+        val conversasFragment = ConversasFragment()
+        val chamadasFragment = ChamadasFragment()
+
+
+        btnConversas.setOnClickListener {
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo, conversasFragment)
+                .commit()
+
+        }
+
+
+        btnChamadas.setOnClickListener {
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo, chamadasFragment)
+//                .remove(conversasFragment)
+                .commit()
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/arttt95/fragments/fragments/ChamadasFragment.kt
@@ -0,0 +1,30 @@
+package com.arttt95.fragments.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.arttt95.fragments.R
+
+class ChamadasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(
+            R.layout.fragment_chamadas,
+            container,
+            false
+        )
+
+        // Processamento da visualização
+
+        return view
+
+    }
+
+}
